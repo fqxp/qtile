4499f4e44668945d0cfd75de0e435dea6d949442
Frank <frank@fqxp.de>
2017-07-21 10:28:15 +0200

    Pomodoro widget: persist timer state

--- libqtile/widget/pomodoro.py
+++ libqtile/widget/pomodoro.py
@@ -21,10 +21,10 @@
 
 from libqtile.log_utils import logger
 from libqtile.widget import base
-import logging
+import datetime
+import os
 import os.path
 import shutil
-import time
 
 
 class Pomodoro(base.InLoopPollText):
@@ -42,87 +42,110 @@ class Pomodoro(base.InLoopPollText):
         ('work_sound', '/usr/share/sounds/freedesktop/stereo/complete.oga', 'Sound to play when starting work interval'),
         ('rest_sound', '/usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga', 'Sound to play when starting rest interval'),
         ('play_sound_command', 'paplay', 'Command to play sound'),
+        ('state_filename', os.path.expanduser('~/.local/share/qtile/pomodoro.timer')),
     ]
 
     def __init__(self, **config):
         base.InLoopPollText.__init__(self, **config)
         self.add_defaults(Pomodoro.defaults)
-        self._running = False
-        self.foreground = self.not_running_text_color
+        self._restore_start_time()
+        self._mode = None
+        self._remaining_time = None
+        self._update_state(silent=True)
+
+    def cmd_toggle(self):
+        if self._is_running():
+            self.cmd_stop()
+        else:
+            self.cmd_start()
+
+    def cmd_start(self):
+        self._start_time = int(datetime.datetime.now().timestamp())
+        self._persist_start_time()
+        self._update_state()
+        self._update_view()
+
+    def cmd_stop(self):
+        self._start_time = None
+        self._persist_start_time()
+        self._update_view()
 
     def tick(self):
+        self._update_state()
         self._update_view()
 
     def button_press(self, x, y, button):
         if button == 1:
             self.cmd_toggle()
 
-    def cmd_toggle(self):
-        if self._running:
-            self.cmd_stop()
+    def _update_state(self, silent=False):
+        if self._is_running():
+            (previous_mode, previous_remaining_time) = (self._mode, self._remaining_time)
+            (self._mode, self._remaining_time) = self._current_state(self._start_time)
+
+            if previous_remaining_time == 1 and self._mode != previous_mode:
+                self._play_sound(self._mode)
         else:
-            self.cmd_start()
-
-    def cmd_start(self):
-        self.cmd_start_interval('work')
-        self._running = True
-        self._update_view()
-
-    def cmd_stop(self):
-        self._running = False
-        self.foreground = self.not_running_text_color
-        self._update_view()
-
-    def cmd_start_interval(self, mode):
-        self._started_at = time.monotonic()
-
-        if mode == 'work':
-            self._mode = 'work'
-            self._current_interval = self.work_interval * 60
-            self.foreground = self.work_text_color
-        else:
-            self._mode = 'rest'
-            self._current_interval = self.rest_interval * 60
-            self.foreground = self.rest_text_color
-
-        self._play_sound(self._mode)
-
-    def cmd_next_interval(self):
-        if self._mode == 'work':
-            next_interval = 'rest'
-        else:
-            next_interval = 'work'
-
-        self.cmd_start_interval(next_interval)
+            self._mode = None
+            self._remaining_time = None
 
     def _update_view(self):
-        if self._running:
-            remaining_time = self._remaining_time(self._started_at, self._current_interval)
-
-            if remaining_time < 1.0:
-                self.cmd_start_interval('work' if self._mode == 'rest' else 'rest')
-                remaining_time = self._current_interval
+        if self._is_running():
+            if self._mode == 'work':
+                self.foreground = self.work_text_color
+            elif self._mode == 'rest':
+                self.foreground = self.rest_text_color
 
             self.update('%s %s' % (
                 self._formatted_mode(self._mode),
-                self._formatted_time(remaining_time)))
+                self._formatted_time(self._remaining_time)))
         else:
+            self.foreground = self.not_running_text_color
             self.update(self.not_running_text)
 
     def _play_sound(self, mode):
         sound = self.work_sound if mode == 'work' else self.rest_sound
         if not os.path.exists(sound):
-            logger.error('Pomodoro: sound file %s does not exist' % sound)
+            logger.error('Sound file %s does not exist' % sound)
             return
 
         if not shutil.which(self.play_sound_command):
-            logger.error('Pomodoro: command "%s" not found in path' % self.play_sound_command)
+            logger.error('Command "%s" not found in path' % self.play_sound_command)
             return
 
         self.qtile.cmd_spawn([self.play_sound_command, sound])
 
-    def _remaining_time(self, started_at, interval):
-        return started_at + interval - time.monotonic()
+    def _persist_start_time(self):
+        if self._is_running():
+            with open(self.state_filename, 'w') as fd:
+                print('%d' % self._start_time, file=fd)
+        else:
+            if os.path.exists(self.state_filename):
+                os.remove(self.state_filename)
+
+    def _restore_start_time(self):
+        if os.path.exists(self.state_filename):
+            try:
+                with open(self.state_filename, 'r') as fd:
+                    self._start_time = int(fd.read())
+            except Exception as e:
+                logger.error('Couldn’t restore start time (%s)' % str(e))
+                self._start_time = None
+        else:
+            self._start_time = None
+
+    def _current_state(self, started_at):
+        now = int(datetime.datetime.now().timestamp())
+        total_interval_seconds = (self.work_interval + self.rest_interval) * 60
+        current_interval_seconds = (now - self._start_time) % total_interval_seconds
+
+        if current_interval_seconds < self.work_interval * 60:
+            return ('work', self.work_interval * 60 - current_interval_seconds)
+        else:
+            return ('rest', self.rest_interval * 60 - (current_interval_seconds - self.work_interval * 60))
+
+    def _is_running(self):
+        return self._start_time is not None
 
     def _formatted_mode(self, mode):
         if mode == 'work':
